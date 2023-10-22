6efb7aaa5f4ae33f5e4196065d44109acfeed871
ruggerofreddichallenge <148683862+ruggerofreddichallenge@users.noreply.github.com>
2023-10-22 17:42:18 +0200

    Add files via upload

---
+++ tables/log_table.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE log_table (
+	[KeyLog] [bigint] IDENTITY(1, 1) NOT NULL CONSTRAINT PK_KeyLog PRIMARY KEY CLUSTERED
+	,[SchemaProc] [nvarchar](100) NULL
+	,[ProcName] [nvarchar](100) NULL
+	,[ExecStatus] [int] NULL
+	,[SQL_ERROR_LINE] [int] NULL
+	,[SQL_ERROR_MESSAGE] [nvarchar](4000) NULL
+	,[SQL_ERROR_NUMBER] [int] NULL
+	,[SQL_ERROR_PROCEDURE] [nvarchar](128) NULL
+	,[SQL_ERROR_SEVERITY] [int] NULL
+	,[SQL_ERROR_STATE] [int] NULL
+	,[StartDate] [datetime] NULL
+	,[EndDate] [datetime] NULL
+	)
